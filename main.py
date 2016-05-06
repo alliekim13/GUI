034af872bfa7283c9cf5343ae0bb80e62249c8f9
Allie Kim <alliekim@dhcp-168-105-84-87.wireless.manoa.hawaii.edu>
2016-05-05 17:06:07 -1000

    Supports separate current and voltage readings

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 import projectsettings
 import os
 import random
+import serial
 import time
 from PyQt4.uic import loadUiType
 from matplotlib.figure import Figure
@@ -31,10 +32,10 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	super(ExampleApp, self).__init__(parent)
 	self.setupUi(self)
 	self.actionNew.triggered.connect(self.new_project)
-	self.startButton.clicked.connect(self.start_log)
+	#self.startButton.clicked.connect(self.start_log)
 	self.fig1 =Figure()
-	self.ax1f1 = self.fig1.add_subplot(111)
-	self.ax1f1.plot(np.random.rand(100))
+	#self.ax1f1 = self.fig1.add_subplot(111)
+	#self.ax1f1.plot(np.random.rand(100))
 	print "Initializing..."    
 	self.actionProject_Settings.triggered.connect(self.openDialog)
 	self.dialog = PopupDialog()
@@ -45,24 +46,27 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	self.timer.timeout.connect(self.read)
 	self.logTimer.timeout.connect(self.logEnd)
 	self.buttonState = False
-	self.addmpl(self.fig1)
+	#self.addmpl(self.fig1)
 	self.logValid = False
+        #self.ser = serial.Serial(port = '/dev/tty.usbmodem1411',baudrate = 9600)
+	self.plotStr = []
 
     def openDialog(self):
-	print "Opening dialog box"
+	#print "Opening dialog box"
 	self.dialog.exec_()
 
     def selectFile(self):
-	print "Selecting file..."
+	#print "Selecting file..."
 	self.dialog.lineEdit_4.setText(QtGui.QFileDialog.getExistingDirectory(self, "Select Directory"))
 
     def save_settings(self):
-	print "Saving data..."
+	#print "Saving data..."
 	self.fileName = self.dialog.lineEdit.text()
-	self.fileName += ".csv"
 	self.logRate = self.dialog.lineEdit_2.text()
 	self.logDuration = self.dialog.lineEdit_3.text()
 	self.filePath = self.dialog.lineEdit_4.text()
+	self.currentLog = self.dialog.radioButton.isChecked()
+	self.voltageLog = self.dialog.radioButton_2.isChecked()
 	if not self.checkData():
 	    print "Data invalid"
 	    self.logValid = False
@@ -72,21 +76,40 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	    self.logDuration = ""
 	    self.filePath = ""
 	    self.show_saveSettingsError()
+	    self.currentLog = False
+	    self.voltageLog = False
+	    try:
+	        self.startButton.clicked.disconnect()
+	    except: pass
+	    self.startButton.clicked.connect(self.show_saveSettingsError)
 	else:
 	    self.logValid = True
+	    try:
+                self.startButton.clicked.disconnect()
+	    except: pass
+	    self.startButton.clicked.connect(self.start_log)
+	    self.fileName += ".csv"
 	    self.outputFilepath = os.path.join(str(self.filePath), str(self.fileName))
-	    print self.outputFilepath
-	self.debug_settings()
+	    if self.currentLog:
+	        self.readingLabel.setText("Current (mA)")
+	    elif self.voltageLog:
+		self.readingLabel.setText("Voltage (V)")
+	#self.debug_settings()
     
     def debug_settings(self):
 	print "Filename: " + self.fileName
 	print "Log rate: " + self.logRate
 	print "Log duration: " + self.logDuration
 	print "File path: " + self.filePath
-    
+
+    def checkRadioButtons(self):
+        if self.currentLog is not self.voltageLog:
+	    return True
+    	else:
+            return False
+	
     def checkData(self):
 	flag = True
-	print "In check data"
 	self.errorStr = ""
 	if not self.fileName:
 	    self.errorStr += "Enter a file name!\n"
@@ -102,6 +125,12 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	    print "Log duration invalid\n"
 	    self.errorStr += "Enter a number for log duration!\n"
 	    flag = False
+        print "Hjasdfkljasdlf"
+	if not self.checkRadioButtons():
+	    print "Radio buttons invalid\n"
+	    self.errorStr += "Select voltage OR current reading!\n"
+	    flag = False
+	print "Finished checking data"
 	return flag
 
     def show_saveSettingsError(self):
@@ -119,8 +148,8 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	if self.buttonState is False:
 	    self.buttonState = True
 	    self.startButton.setText("Stop")
-	    self.timer.start(int(self.logRate)*1000)
-	    self.logTimer.start(int(self.logDuration)*1000)
+	    self.timer.start(float(self.logRate)*1000)
+	    self.logTimer.start(float(self.logDuration)*1000)
 	elif self.buttonState is True:
 	    self.buttonState = False
 	    self.startButton.setText("Start")
@@ -138,12 +167,18 @@ class ExampleApp(QtGui.QMainWindow, design2.Ui_MainWindow):
 	self.timer.stop()
 	self.buttonState = False
 	self.startButton.setText("Start")
-
+        self.ax1f1 = self.fig1.add_subplot(111)
+        self.ax1f1.plot(self.plotStr)
+        self.addmpl(self.fig1) 
     def read(self):
 	#function that gets called when qtimer expires
 	#replace this line with code to do readouts
-	reading = random.random()
-	print reading
+	if self.currentLog:
+	    reading = random.random() #insert current reading here
+	elif self.voltageLog:
+	    reading = random.random() #insert voltage reading here
+	self.plotStr.append(reading)
+	#print reading
 	self.lcdNumber_2.display(reading)
 	self.dataloglist.addItem(str(reading))
 	dt = datetime.datetime.now()
